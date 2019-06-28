8b5b58f99fbdc9c4f3ba197843b7c71bd6ffbb93
Max Froumentin <github@lapin-bl.eu>
2019-06-28 17:59:12 +0100

    Make it record and display timestamps

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 (function() {
 
+  const appState = {
+    timestamps: []
+  };
+
+  const addTimestamp = function() {
+    appState.timestamps.push(new Date());
+  };
+
+  const formatTimestamp = function(d) {
+    return `${d.getDate()}/${d.getMonth()} - ${d.getHours()}:${d.getMinutes()}`;
+  };
+
+  const reset = function() {
+    appState.timestamps = [];
+  }
+
   const init = function() {
     new Vue({
       el: '#app',
-      data: {
-        message: 'Hello Vue.js!'
-      }
+      data: appState,
+      methods: { addTimestamp, formatTimestamp, reset }
     });
   };
 
   document.addEventListener('readystatechange', event => {
-    if (event.target.readyState === 'complete') {
+    if (event.target.readyState === 'interactive') {
       init();
     }
   });
