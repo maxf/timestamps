facb0c9e5337e084b5e7391ae1cf923ba91fbc2c
Max Froumentin <github@lapin-bl.eu>
2019-07-01 18:03:48 +0100

    let user change timestamp before adding

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 (function() {
 
   const appState = {
-    timestamps: []
+    timestamps: [],
+    newTimestamp: {
+      date: '',
+      month: '',
+      year: '',
+      hours: '',
+      minutes: ''
+    }
   };
 
+  const prepareNewTimestamp = function() {
+    const now = new Date();
+    appState.newTimestamp = {
+      date: now.getDate(),
+      month: now.getMonth(),
+      year: now.getYear(),
+      hours: now.getHours(),
+      minutes: now.getMinutes()
+    };
+  }
+
   const addTimestamp = function() {
-    appState.timestamps.push(new Date());
+    appState.timestamps.push(appState.newTimestamp);
   };
 
   const formatTimestamp = function(d) {
-    return `${d.getDate()}/${d.getMonth()} - ${d.getHours()}:${d.getMinutes()}`;
+    return `${d.date}/${d.month} - ${d.hours}:${d.minutes}`;
   };
 
   const reset = function() {
@@ -20,7 +38,7 @@
     new Vue({
       el: '#app',
       data: appState,
-      methods: { addTimestamp, formatTimestamp, reset }
+      methods: { addTimestamp, formatTimestamp, prepareNewTimestamp, reset }
     });
   };
 
